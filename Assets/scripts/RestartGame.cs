f778a87ec129c525839659ea88f10538f57016da
MFDoomShroom <dhuste25@student.aau.dk>
2025-12-02 10:58:22 +0100

    Changes to UI + larger text
    Større tekst til bars og mere i øjenfaldene UI

--- Assets/scripts/RestartGame.cs
+++ Assets/scripts/RestartGame.cs
@@ -8,7 +8,7 @@ public class Restart : MonoBehaviour
         // Nulstil score hvis I vil starte forfra
         staticBarIntergers.climateBar = 70;
         staticBarIntergers.justiceBar = 0;
-        staticBarIntergers.EconomyBar = 3;
+        staticBarIntergers.EconomyBar = 30;
         characterManager.currentCharacterIndex = 0;
         // Load spillet igen
         SceneManager.LoadScene("jeppeScene");
--- Assets/scripts/staticBarIntergers.cs
+++ Assets/scripts/staticBarIntergers.cs
@@ -2,5 +2,5 @@ public static class staticBarIntergers
 {
     public static int climateBar = 70;
     public static int justiceBar = 0;
-    public static int EconomyBar = 3; 
+    public static int EconomyBar = 30; 
 }
