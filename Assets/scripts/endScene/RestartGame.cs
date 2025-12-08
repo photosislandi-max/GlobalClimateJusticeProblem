13ceae67541330ec4a5e77c643fe49287c729487
Bezenticz <thaaprinzz@gmail.com>
2025-12-08 12:19:28 +0100

    Intro Video, UI sound

--- Assets/scripts/endScene/RestartGame.cs
+++ Assets/scripts/endScene/RestartGame.cs
@@ -19,6 +19,6 @@ public class RestartGame : MonoBehaviour
         staticBarIntergers.climateBar = 70;
         staticBarIntergers.justiceBar = 0; 
         staticBarIntergers.EconomyBar = 3;
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("chrisSceneX");
     }
 }
